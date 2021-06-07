0564172ea662ca099dee99912733d2814040bbcc
0wv <w32w64@gmail.com>
2021-06-07 11:08:58 +0000

    :wrench: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,19 @@ module.exports = {
   extends: [
     'standard'
   ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module'
   },
+  plugins: [
+    'svelte3'
+  ],
   rules: {
   }
 }
