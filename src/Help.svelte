941c9781909088c68334cf18880b5c607ffd1b8f
0wv <w32w64@gmail.com>
2021-06-07 09:30:00 +0000

    :sparkles: add example in help

--- src/Help.svelte
+++ src/Help.svelte
@@ -1,2 +1,18 @@
+<script>
+    const fetchExample = (async () => {
+        const responce = await fetch("/example-qa.txt");
+        const result = await responce.text();
+
+        return result;
+    })();
+</script>
+
 <button onclick="window.location.href = '/#/'">戻る</button>
 <h1>ヘルプ</h1>
+{#await fetchExample}
+    <p>...waiting</p>
+{:then text}
+    <pre>
+    <code>{text}</code>
+</pre>
+{/await}
