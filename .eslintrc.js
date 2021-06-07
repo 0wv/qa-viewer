a9161949f64db0674f8ff2ea55bf8d2f46e5afc9
0wv <w32w64@gmail.com>
2021-06-07 12:00:05 +0000

    :wrench: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     'svelte3'
   ],
   rules: {
+    // ref: https://github.com/sveltejs/eslint-plugin-svelte3/blob/master/OTHER_PLUGINS.md#eslint-plugin-import
+    'import/first': 'off',
     // ref: https://github.com/sveltejs/eslint-plugin-svelte3/issues/41#issuecomment-572503966
     'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 2, maxEOF: 0 }] // standard
   }
