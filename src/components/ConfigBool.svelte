c7a0a6c5be734a8df857cada17f9f2dd31686369
0wv <w32w64@gmail.com>
2021-06-09 12:27:49 +0000

    設定のストアを追加 Fixes #11

---
+++ src/components/ConfigBool.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { config } from '../stores.js'
+
+  export let key
+  export let value = false
+
+  $config[key] = value
+</script>
+
+<label>
+  <input bind:checked={$config[key]} type="checkbox" />
+  <slot></slot>
+</label>
