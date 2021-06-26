e55d4992ed5bbf5a33013dae56571be99d3a164a
0wv <w32w64@gmail.com>
2021-06-26 06:26:04 +0000

    :recycle: refactor code

--- src/parser.test.js
+++ src/parser.test.js
@@ -108,6 +108,6 @@ test('unescape for qas', () => {
 
 test('allow line breaks inside lines', () => {
   expect(
-    new QAString('text:+un &\ndeux &\ntrois').format().items[0].content
+    (new QAString('text:+un &\ndeux &\ntrois')).format().items[0].content
   ).toBe('un deux trois')
 })
