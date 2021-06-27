31e2d27d8f8c5d36160ce9a5fbcbf4e51ef7903b
0wv <w32w64@gmail.com>
2021-06-27 00:26:54 +0000

    :lipstick: update look

--- src/components/Answer.svelte
+++ src/components/Answer.svelte
@@ -7,10 +7,10 @@
 </script>
 
 <div class="bg-green-50 p-2 pt-4 rounded-2xl shadow">
-  <span class="bg-green-200 p-2 rounded-2xl shadow-inner">答え</span>
+  <span class="bg-green-200 px-4 py-2 rounded-2xl shadow-inner">答え</span>
   <ul class="mt-4">
     {#each qa.content.answers as answer}
-    <li class="bg-green-100 mt-2 p-4 rounded-2xl shadow-inner">
+    <li class="bg-green-100 mt-2 px-4 py-2 rounded-2xl shadow-inner">
       {#if !$config.isEnableInnerHTML}
       {QAString.unescape(answer)}
       {:else}
--- src/components/Question.svelte
+++ src/components/Question.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="bg-blue-50 mb-3 p-4 pl-2 rounded-2xl shadow">
-  <span class="bg-blue-200 mr-2 p-2 rounded-2xl shadow-inner">問 {qa.meta.questionIndex}</span>
+  <span class="bg-blue-200 mr-2 px-4 py-2 rounded-2xl shadow-inner">問 {qa.meta.questionIndex}</span>
   {#if !$config.isEnableInnerHTML}
   {#if qa.type !== 'fill'}
   {QAString.unescape(qa.content.question)}
