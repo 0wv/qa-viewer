f0479d3e193221b7ba38bc0e1306367fb0831dcc
0wv <w32w64@gmail.com>
2021-06-27 01:09:21 +0000

    :lipstick: update look

--- src/components/Answer.svelte
+++ src/components/Answer.svelte
@@ -6,7 +6,7 @@
   export let qa
 </script>
 
-<div class="bg-green-50 p-2 pt-4 rounded-2xl shadow">
+<div class="bg-green-50 leading-loose p-2 pt-4 rounded-2xl shadow">
   <span class="bg-green-200 px-4 py-2 rounded-2xl shadow-inner">答え</span>
   <ul class="mt-4">
     {#each qa.content.answers as answer}
--- src/components/Question.svelte
+++ src/components/Question.svelte
@@ -6,14 +6,16 @@
   export let qa
 </script>
 
-<div class="bg-blue-50 mb-3 p-4 pl-2 rounded-2xl shadow">
+<div class="bg-blue-50 leading-loose mb-3 p-4 pl-2 rounded-2xl shadow">
   <span class="bg-blue-200 mr-2 px-4 py-2 rounded-2xl shadow-inner">問 {qa.meta.questionIndex}</span>
   {#if !$config.isEnableInnerHTML}
-  {#if qa.type !== 'fill'}
-  {QAString.unescape(qa.content.question)}
-  {:else}
-  {QAString.unescape(qa.content.text.replace(/\(\(.+?\)\)/, '()'))}
-  {/if}
+  <span class="break-words">
+    {#if qa.type !== 'fill'}
+    {QAString.unescape(qa.content.question)}
+    {:else}
+    {QAString.unescape(qa.content.text.replace(/\(\(.+?\)\)/, '()'))}
+    {/if}
+  </span>
   {:else}
   {#if qa.type !== 'fill'}
   <HTMLCode value={QAString.unescape(qa.content.question)}></HTMLCode>
