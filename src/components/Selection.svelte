fab3a1f9c62b3fd336827ca012ac7cfae0542292
0wv <w32w64@gmail.com>
2021-06-27 01:19:09 +0000

    :lipstick: add selection component

---
+++ src/components/Selection.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { QAString } from '../parser'
+  import { config } from '../stores'
+  import HTMLCode from './HTMLCode.svelte'
+
+  export let qa
+</script>
+
+<div class="bg-yellow-50 leading-loose mb-3 p-2 pt-4 rounded-2xl shadow">
+  <span class="bg-yellow-200 px-4 py-2 rounded-2xl shadow-inner">選択肢</span>
+  <ol class="mt-4">
+    {#each qa.content.selections as selection}
+    <li class="bg-yellow-100 mt-2 px-4 py-2 rounded-2xl shadow-inner">
+      {#if !$config.isEnableInnerHTML}
+      {QAString.unescape(selection)}
+      {:else}
+      <HTMLCode value={QAString.unescape(selection)}></HTMLCode>
+      {/if}
+    </li>
+    {/each}
+  </ol>
+</div>
--- src/components/prefixes/QAPrefix.svelte
+++ src/components/prefixes/QAPrefix.svelte
@@ -1,9 +1,8 @@
 <script>
-  import { QAString } from '../../parser'
   import { config } from '../../stores'
   import Answer from '../Answer.svelte'
-  import HTMLCode from '../HTMLCode.svelte'
   import Question from '../Question.svelte'
+  import Selection from '../Selection.svelte'
 
   export let qa
 </script>
@@ -20,18 +19,7 @@
   {:else if qa.content.type === 'exact-match-selection'}
   <Question {qa}></Question>
   {#if !$config.isHiddenSelection}
-  <p><span style="font-weight: bold;">＜選択肢＞</span></p>
-  <ol>
-    {#each qa.content.selections as selection}
-    <li>
-      {#if !$config.isEnableInnerHTML}
-      {QAString.unescape(selection)}
-      {:else}
-      <HTMLCode value={QAString.unescape(selection)}></HTMLCode>
-      {/if}
-    </li>
-    {/each}
-  </ol>
+  <Selection {qa}></Selection>
   {/if}
   {#if $config.isAnswerForm}
   <div class:answer-form-border={$config.isAnswerFormBorder} style="height: 2cm; margin-bottom: 1rem;" />
