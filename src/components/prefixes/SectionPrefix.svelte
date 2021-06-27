116be668f3f52f200c562e95c61180f6b07184d1
0wv <w32w64@gmail.com>
2021-06-27 02:00:21 +0000

    :lipstick: update components

--- src/components/prefixes/SectionPrefix.svelte
+++ src/components/prefixes/SectionPrefix.svelte
@@ -6,12 +6,10 @@
   export let qa
 </script>
 
-<div>
-  <hr>
+<div class="border-4 border-purple-200 bg-purple-50 leading-loose mb-6 p-4 pl-2 rounded-2xl shadow">
   {#if !$config.isEnableInnerHTML}
   {QAString.unescape(qa.content)}
   {:else}
   <HTMLCode value={QAString.unescape(qa.content)}></HTMLCode>
   {/if}
-  <hr>
 </div>
--- src/components/prefixes/TextPrefix.svelte
+++ src/components/prefixes/TextPrefix.svelte
@@ -6,12 +6,10 @@
   export let qa
 </script>
 
-<div>
-  <p>
-    {#if !$config.isEnableInnerHTML}
-    {QAString.unescape(qa.content)}
-    {:else}
-    <HTMLCode value={QAString.unescape(qa.content)}></HTMLCode>
-    {/if}
-  </p>
+<div class="border-4 border-gray-200 bg-gray-50 leading-loose mb-6 p-4 pl-2 rounded-2xl shadow">
+  {#if !$config.isEnableInnerHTML}
+  {QAString.unescape(qa.content)}
+  {:else}
+  <HTMLCode value={QAString.unescape(qa.content)}></HTMLCode>
+  {/if}
 </div>
