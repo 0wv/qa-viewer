6d6543aee09b8382ac507c87d1c837bcf3d55251
0wv <w32w64@gmail.com>
2021-07-05 13:51:14 +0000

    :lipstick: add index to input area

--- src/components/InputAnswer.svelte
+++ src/components/InputAnswer.svelte
@@ -9,11 +9,16 @@
   <ul class="mt-2">
     {#each qa.content.answers as _, i}
     <li>
-      <input
-        bind:value={$user.answers[qa.meta.questionIndex - 1][i]}
-        class="bg-indigo-100 mt-2 border-0 px-4 py-2 rounded-2xl shadow-inner w-full"
-        type="text"
-      >
+      <div class="flex">
+        <div class="bg-gradient-to-br flex-initial from-blue-200 mr-2 mt-2 px-4 py-2 rounded shadow-md to-indigo-200">{i + 1}</div>
+        <div class="flex-auto">
+          <input
+            bind:value={$user.answers[qa.meta.questionIndex - 1][i]}
+            class="bg-indigo-100 mt-2 border-0 px-4 py-2 rounded-2xl shadow-inner w-full"
+            type="text"
+          >
+        </div>
+      </div>
     </li>
     {/each}
   </ul>
