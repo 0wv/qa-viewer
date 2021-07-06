47e0dbdf8666e9fb32e1b0ba66adcf15a2afb6cc
0wv <w32w64@gmail.com>
2021-07-06 07:51:47 +0000

    :lipstick: add gradient background

--- src/components/Play.svelte
+++ src/components/Play.svelte
@@ -81,111 +81,113 @@
   }
 </script>
 
-{#if $user.answers.length !== currentIndex}
-<nav class="bg-gray-50 flex mb-3 px-4 py-2 shadow">
-  <ul>
-    <li>
-      <button class="bg-gray-200 px-4 py-2 rounded-full" onclick="window.location.href = '/#/'">戻る</button>
-    </li>
-  </ul>
-</nav>
-{#if filteredQAs[currentIndex].type === 'fill'}
-<div class="mb-3 mx-4">
-  <Question qa={filteredQAs[currentIndex]}></Question>
-  <InputAnswer qa={filteredQAs[currentIndex]}></InputAnswer>
-</div>
-{#if !isShowAnswer}
-<div class="mx-4">
-  <button class="bg-gray-50 px-4 py-2 rounded-full shadow-md w-full" on:click={okClick}>OK</button>
-</div>
-{:else}
-<p>
-  {#if checkAnswer()}
-  正解です！
+<div class="bg-gradient-to-br from-indigo-300 h-full to-blue-300 w-full">
+  {#if $user.answers.length !== currentIndex}
+  <nav class="bg-gray-50 flex mb-3 px-4 py-2 shadow">
+    <ul>
+      <li>
+        <button class="bg-gray-200 px-4 py-2 rounded-full" onclick="window.location.href = '/#/'">戻る</button>
+      </li>
+    </ul>
+  </nav>
+  {#if filteredQAs[currentIndex].type === 'fill'}
+  <div class="mb-3 mx-4">
+    <Question qa={filteredQAs[currentIndex]}></Question>
+    <InputAnswer qa={filteredQAs[currentIndex]}></InputAnswer>
+  </div>
+  {#if !isShowAnswer}
+  <div class="mx-4">
+    <button class="bg-gray-50 px-4 py-2 rounded-full shadow-md w-full" on:click={okClick}>OK</button>
+  </div>
   {:else}
-  不正解です…
+  <p>
+    {#if checkAnswer()}
+    正解です！
+    {:else}
+    不正解です…
+    {/if}
+  </p>
+  <button on:click={nextQuestion}>次の問題</button>
   {/if}
-</p>
-<button on:click={nextQuestion}>次の問題</button>
-{/if}
-{:else if filteredQAs[currentIndex].type === 'qa'}
-{#if filteredQAs[currentIndex].content.type === 'exact-match'}
-<p>
-  <Question qa={filteredQAs[currentIndex]}></Question>
-</p>
-<textarea bind:value={$user.answers[currentIndex]} placeholder="答えを入力してください" />
-{#if !isShowAnswer}
-<br />
-<button on:click={okClick}>OK</button>
-{:else}
-<p>
-  {#if checkAnswer()}
-  正解です！
+  {:else if filteredQAs[currentIndex].type === 'qa'}
+  {#if filteredQAs[currentIndex].content.type === 'exact-match'}
+  <p>
+    <Question qa={filteredQAs[currentIndex]}></Question>
+  </p>
+  <textarea bind:value={$user.answers[currentIndex]} placeholder="答えを入力してください" />
+  {#if !isShowAnswer}
+  <br />
+  <button on:click={okClick}>OK</button>
   {:else}
-  不正解です…
+  <p>
+    {#if checkAnswer()}
+    正解です！
+    {:else}
+    不正解です…
+    {/if}
+  </p>
+  <button on:click={nextQuestion}>次の問題</button>
   {/if}
-</p>
-<button on:click={nextQuestion}>次の問題</button>
-{/if}
-{:else if filteredQAs[currentIndex].content.type === 'exact-match-selection'}
-<p><Question qa={filteredQAs[currentIndex]}></Question></p>
-{#if filteredQAs[currentIndex].content.answers.length === 1}
-<p>答えをひとつ選択してください</p>
-{#each filteredQAs[currentIndex].content.selections as selection, i}
-<label>
-  <input bind:group={$user.answers[currentIndex]} type="radio" value={i} />
-  {#if !$config.isEnableInnerHTML}
-  {selection}
-  {:else}
-  <span bind:innerHTML={selection} contenteditable="true"></span>
-  {/if}
-</label>
-{/each}
-{#if !isShowAnswer}
-<button on:click={okClick}>OK</button>
-{:else}
-<p>
-  {#if checkAnswer()}
-  正解です！
-  {:else}
-  不正解です…
-  {/if}
-</p>
-<button on:click={nextQuestion}>次の問題</button>
-{/if}
-{:else}
-<p>答えを選択してください</p>
-{#each filteredQAs[currentIndex].content.selections as selection}
-<label>
-  <input bind:group={$user.answers[currentIndex]} type="checkbox" value={selection} />
-  {#if !$config.isEnableInnerHTML}
-  {selection}
-  {:else}
-  <span bind:innerHTML={selection} contenteditable="true"></span>
-  {/if}
-</label>
-{/each}
-{#if !isShowAnswer}
-<button on:click={okClick}>OK</button>
-{:else}
-<p>
-  {#if checkAnswer()}
-  正解です！
-  {:else}
-  不正解です…
-  {/if}
-</p>
-<button on:click={nextQuestion}>次の問題</button>
-{/if}
-{/if}
-{/if}
-{/if}
-{:else}
-<p>＜結果＞</p>
-<ol>
-  {#each filteredQAs as _, i}
-  <li>{$user.results[i] ? '正解' : '不正解'}</li>
+  {:else if filteredQAs[currentIndex].content.type === 'exact-match-selection'}
+  <p><Question qa={filteredQAs[currentIndex]}></Question></p>
+  {#if filteredQAs[currentIndex].content.answers.length === 1}
+  <p>答えをひとつ選択してください</p>
+  {#each filteredQAs[currentIndex].content.selections as selection, i}
+  <label>
+    <input bind:group={$user.answers[currentIndex]} type="radio" value={i} />
+    {#if !$config.isEnableInnerHTML}
+    {selection}
+    {:else}
+    <span bind:innerHTML={selection} contenteditable="true"></span>
+    {/if}
+  </label>
   {/each}
-</ol>
-<button onclick="window.location.href = '/#/'">戻る</button>
-{/if}
+  {#if !isShowAnswer}
+  <button on:click={okClick}>OK</button>
+  {:else}
+  <p>
+    {#if checkAnswer()}
+    正解です！
+    {:else}
+    不正解です…
+    {/if}
+  </p>
+  <button on:click={nextQuestion}>次の問題</button>
+  {/if}
+  {:else}
+  <p>答えを選択してください</p>
+  {#each filteredQAs[currentIndex].content.selections as selection}
+  <label>
+    <input bind:group={$user.answers[currentIndex]} type="checkbox" value={selection} />
+    {#if !$config.isEnableInnerHTML}
+    {selection}
+    {:else}
+    <span bind:innerHTML={selection} contenteditable="true"></span>
+    {/if}
+  </label>
+  {/each}
+  {#if !isShowAnswer}
+  <button on:click={okClick}>OK</button>
+  {:else}
+  <p>
+    {#if checkAnswer()}
+    正解です！
+    {:else}
+    不正解です…
+    {/if}
+  </p>
+  <button on:click={nextQuestion}>次の問題</button>
+  {/if}
+  {/if}
+  {/if}
+  {/if}
+  {:else}
+  <p>＜結果＞</p>
+  <ol>
+    {#each filteredQAs as _, i}
+    <li>{$user.results[i] ? '正解' : '不正解'}</li>
+    {/each}
+  </ol>
+  <button onclick="window.location.href = '/#/'">戻る</button>
+  {/if}
+</div>
