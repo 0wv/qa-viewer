8cdef910d2b004d5d1a00e848d4b0173104317e4
0wv <w32w64@gmail.com>
2021-07-10 12:59:54 +0900

    :lipstick: update look

--- src/components/InputAnswer.svelte
+++ src/components/InputAnswer.svelte
@@ -26,7 +26,7 @@
   {:else if qa.type === 'qa' && qa.content.type === 'exact-match'}
   <input
     bind:value={$user.answers[qa.meta.questionIndex - 1]}
-    class="bg-indigo-100 mt-2 border-0 px-4 py-2 rounded-2xl shadow-inner w-full"
+    class="bg-indigo-100 mt-4 border-0 px-4 py-2 rounded-2xl shadow-inner w-full"
     type="text"
   >
   {/if}
