2a87ca733902540a17ac4d66e61197e7333853c7
0wv <w32w64@gmail.com>
2021-07-11 22:07:46 +0900

    :lipstick: make section font larger and bold

--- src/components/prefixes/SectionPrefix.svelte
+++ src/components/prefixes/SectionPrefix.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div
-  class="border-4 border-purple-200 bg-purple-50 leading-loose mb-6 mx-4 p-4 pl-2 question rounded-2xl shadow"
+  class="border-4 border-purple-200 bg-purple-50 font-bold leading-loose mb-6 mx-4 p-4 pl-2 question rounded-2xl shadow text-lg"
   style="-webkit-print-color-adjust: exact;"
 >
   {#if !$config.isEnableInnerHTML}
