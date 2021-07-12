4a0e2d892bc0f14411516402ba9827d6fcc060d8
0wv <w32w64@gmail.com>
2021-07-13 07:18:09 +0900

    :sparkles: update message

--- src/components/Answer.svelte
+++ src/components/Answer.svelte
@@ -15,7 +15,11 @@
     {#each qa.content.answers as answer}
     <li class="bg-green-100 mt-2 px-4 py-2 rounded-2xl shadow-inner">
       {#if !$config.isEnableInnerHTML}
+      {#if qa.content.type !== 'exact-match-selection'}
       {QAString.unescape(answer)}
+      {:else}
+      {QAString.unescape(qa.content.selections[answer - 1])}
+      {/if}
       {:else}
       {#if qa.content.type === 'exact-match-selection'}
       <HTMLCode value={qa.content.selections[answer - 1]}></HTMLCode>
